550e77a46ee23a06135ee38291cf5d6c2951a376
Liu <retamaliu@gmail.com>
2022-12-19 12:35:08 +0100

    actualizacion

--- juego/js/juego.js
+++ juego/js/juego.js
@@ -1,13 +1,26 @@
-$(principal) ;
+$(principal);
 function principal(){
-    const t = $("tablero");
-
-    const letras = "kljasdhf";
-
+    const letras = "kljasdhf";    
     for (var i = 0; i < letras.length; i++) {
-        t.append("<span >"+letras[i]+"</span>");
+        $("#tablero").append("<span id='arrastrable"+i+"' draggable='true' ondragend='finArrastre(event)'>"+letras[i]+"</span>");
+        //Se añaden los eventos para mover cada letra
+        //Importante poner # para que lo coja del html
     }
 }
+
+//Solo se necesita funcion ondragend. 
+//Al arrastrar las letras siempre se arrastrarán en el #tablero al final
+function finArrastre(event){
+    $("#tablero").append($("#"+ event.target.id))
+    //Al tablero se le añade el id de cada span
+}
+
 function comprobar(){
-    console.log("comprueba")
+    let letra = $("#tablero").text() //Coje el valor del tablero (solo el texto dentro de los span)
+    for (let i = 1; i < letra.length; i++) { 
+        if(letra[i]<letra[i-1]){
+            return alert("La palabra no está ordenada.")
+        }        
+    }
+    return alert("La palabra está ordenada.")
 }
