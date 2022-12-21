18ca638f3cb68d05eecfc8f4913677735ea50ef3
Liu <retamaliu@gmail.com>
2022-12-21 10:45:05 +0100

    a

--- juego/js/juego.js
+++ juego/js/juego.js
@@ -1,11 +1,12 @@
 $(principal);
 function principal(){
-    const letras = "kljasdhf";    
+    const letras = "nba";    
     for (var i = 0; i < letras.length; i++) {
         $("#tablero").append("<span id='arrastrable"+i+"' draggable='true' ondragend='finArrastre(event)'>"+letras[i]+"</span>");
         //Se añaden los eventos para mover cada letra
         //Importante poner # para que lo coja del html
     }
+    comprobar();
 }
 
 //Solo se necesita funcion ondragend. 
@@ -13,14 +14,20 @@ function principal(){
 function finArrastre(event){
     $("#tablero").append($("#"+ event.target.id))
     //Al tablero se le añade el id de cada span
+    comprobar();
 }
 
 function comprobar(){
-    let letra = $("#tablero").text() //Coje el valor del tablero (solo el texto dentro de los span)
-    for (let i = 1; i < letra.length; i++) { 
-        if(letra[i]<letra[i-1]){
-            return alert("La palabra no está ordenada.")
+    //$("#tablero").text() -> Coje el valor del tablero (solo el texto dentro de los span)
+    let ordenado=true;
+    for (let i = 1; i <  $("#tablero").text().length; i++) { 
+        if( $("#tablero").text()[i]< $("#tablero").text()[i-1]){
+            ordenado=false;              
         }        
     }
-    return alert("La palabra está ordenada.")
+    if(ordenado){
+        $("#tablero").css('color', '#0CC722') //Si está ordenado se pinta en verde
+    }else{
+        $("#tablero").css('color', 'red') //Si está mal ordenado se pinta en rojo
+    }
 }
